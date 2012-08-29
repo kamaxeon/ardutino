ff61de2bf7a203456c01aa5ef3ddee7cf186b240
Israel Santana <kamaxeon@users.noreply.github.com>
2012-08-29 11:09:52 +0000

    Subiendo la nueva estructura

---
+++ trunk/Arduino/Librerías/ArduTino/Camara.cpp
@@ -0,0 +1,305 @@
+// Librería Camara
+// 
+// Copyright 2012 Israel Santana <isra@miscorreos.org>
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+// MA 02110-1301, USA.
+//
+/*
+ Basado la parte de los sensores en la libería SHT1X de Jonathan Oxer
+ y Maurice Ribble (también GPL)
+
+ La idea de esta librería es tener centralizado lo que puede hacer una 
+ cámara.
+
+*/
+
+#if (ARDUINO >= 100)
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+#include "Camara.h"
+
+
+/* ================  Constructor ================ */
+
+Camara::Camara(int pinEstado, int pinApagado, int pinDatosSensor, int pinRelojSensor)
+{
+	// El estado de la camara al iniciarse siempre es true
+	bool _estadoAntiguo = true;
+	
+	_pinEstado	= pinEstado;
+	_pinApagado	=	pinApagado;
+	_dataPin 		= pinDatosSensor;
+	_clockPin 	= pinRelojSensor;
+	
+}
+
+/* ================  Métodos Públicos ================ */
+
+
+int Camara::leerTemperatura()
+{
+  int _val; // Raw value returned from sensor
+  float _temperature; // Temperature derived from raw value
+
+  // Conversion coefficients from SHT15 datasheet
+  const float D1 = -40.0; // for 14 Bit @ 5V
+  const float D2 = 0.01; // for 14 Bit DEGC
+
+  // Fetch raw value
+  _val = readTemperatureRaw();
+
+  // Convert raw value to degrees Celsius
+  _temperature = (_val * D2) + D1;
+
+  return (int)(_temperature);
+}
+
+/**
+* Lee la humedad y la devuelve un entero
+*/
+
+int Camara::leerHumedad()
+{
+  int _val; // Raw humidity value returned from sensor
+  float _linearHumidity; // Humidity with linear correction applied
+  float _correctedHumidity; // Temperature-corrected humidity
+  float _temperature; // Raw temperature value
+
+  // Conversion coefficients from SHT15 datasheet
+  const float C1 = -4.0; // for 12 Bit
+  const float C2 = 0.0405; // for 12 Bit
+  const float C3 = -0.0000028; // for 12 Bit
+  const float T1 = 0.01; // for 14 Bit @ 5V
+  const float T2 = 0.00008; // for 14 Bit @ 5V
+
+  // Command to send to the SHT1x to request humidity
+  int _gHumidCmd = 0b00000101;
+
+  // Fetch the value from the sensor
+  sendCommandSHT(_gHumidCmd, _dataPin, _clockPin);
+  waitForResultSHT(_dataPin);
+  _val = getData16SHT(_dataPin, _clockPin);
+  skipCrcSHT(_dataPin, _clockPin);
+
+  // Apply linear conversion to raw value
+  _linearHumidity = C1 + C2 * _val + C3 * _val * _val;
+
+  // Get current temperature for humidity correction
+  _temperature = leerTemperatura();
+
+  // Correct humidity value for current temperature
+  _correctedHumidity = (_temperature - 25.0 ) * (T1 + T2 * _val) + _linearHumidity;
+
+  return (int)(_correctedHumidity);
+}
+
+/**
+* Apaga la cámara, se hace poniendo un pin activo durante 3 segundos
+*/
+void Camara::apagar()
+{
+	pinMode(_pinApagado, OUTPUT); // Ponemos el pin de salida
+	
+	digitalWrite(_pinApagado, HIGH);
+	delay(3000);
+	digitalWrite(_pinApagado, LOW);
+}
+/**
+* Comprueba el estado de la cámara, devuelve true si está encendia
+* y false si está apagada
+*/
+bool Camara::leerEstado()
+{
+	pinMode(_pinEstado, INPUT); // Ponemos el pin de entrada
+	
+	int lectura; // Defino el valor para leer el estado del pin
+	
+	lectura = digitalRead(_pinEstado); // leo el estado del pin
+	
+	if ( lectura == HIGH )
+	{
+		return true; // la cámara está encendida
+	}
+	else
+	{
+		return false; // la cámara está apagada
+	}
+	
+}
+
+/**
+* Comprueba los cambios estado de la cámara, devuelve true si ha
+* cambiado, y false si el estado es igual al anterior
+*/
+bool comprobarCambioEstado()
+{
+	bool estadoNuevo; // Variable para comparar
+
+	estadoNuevo = leerEstado();
+
+	if (estadoNuevo != _estadoAntiguo)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+	_estadoAntiguo = estadoNuevo; // Guardamos el nuevo estado
+}
+
+/* ================ Private methods ================ */
+
+
+/* 
+ * Copiado directamente del original SHT1X
+ */
+/**
+* Reads the current raw temperature value
+*/
+float Camara::readTemperatureRaw()
+{
+  int _val;
+
+  // Command to send to the SHT1x to request Temperature
+  int _gTempCmd = 0b00000011;
+
+  sendCommandSHT(_gTempCmd, _dataPin, _clockPin);
+  waitForResultSHT(_dataPin);
+  _val = getData16SHT(_dataPin, _clockPin);
+  skipCrcSHT(_dataPin, _clockPin);
+
+  return (_val);
+}
+
+/**
+*/
+int Camara::shiftIn(int _dataPin, int _clockPin, int _numBits)
+{
+  int ret = 0;
+  int i;
+
+  for (i=0; i<_numBits; ++i)
+  {
+     digitalWrite(_clockPin, HIGH);
+     delay(10); // I don't know why I need this, but without it I don't get my 8 lsb of temp
+     ret = ret*2 + digitalRead(_dataPin);
+     digitalWrite(_clockPin, LOW);
+  }
+
+  return(ret);
+}
+
+/**
+*/
+void Camara::sendCommandSHT(int _command, int _dataPin, int _clockPin)
+{
+  int ack;
+
+  // Transmission Start
+  pinMode(_dataPin, OUTPUT);
+  pinMode(_clockPin, OUTPUT);
+  digitalWrite(_dataPin, HIGH);
+  digitalWrite(_clockPin, HIGH);
+  digitalWrite(_dataPin, LOW);
+  digitalWrite(_clockPin, LOW);
+  digitalWrite(_clockPin, HIGH);
+  digitalWrite(_dataPin, HIGH);
+  digitalWrite(_clockPin, LOW);
+
+  // The command (3 msb are address and must be 000, and last 5 bits are command)
+  shiftOut(_dataPin, _clockPin, MSBFIRST, _command);
+
+  // Verify we get the correct ack
+  digitalWrite(_clockPin, HIGH);
+  pinMode(_dataPin, INPUT);
+  ack = digitalRead(_dataPin);
+  if (ack != LOW) {
+    //Serial.println("Ack Error 0");
+  }
+  digitalWrite(_clockPin, LOW);
+  ack = digitalRead(_dataPin);
+  if (ack != HIGH) {
+    //Serial.println("Ack Error 1");
+  }
+}
+
+/**
+*/
+void Camara::waitForResultSHT(int _dataPin)
+{
+  int i;
+  int ack;
+
+  pinMode(_dataPin, INPUT);
+
+  for(i= 0; i < 100; ++i)
+  {
+    delay(10);
+    ack = digitalRead(_dataPin);
+
+    if (ack == LOW) {
+      break;
+    }
+  }
+
+  if (ack == HIGH) {
+    //Serial.println("Ack Error 2"); // Can't do serial stuff here, need another way of reporting errors
+  }
+}
+
+/**
+*/
+int Camara::getData16SHT(int _dataPin, int _clockPin)
+{
+  int val;
+
+  // Get the most significant bits
+  pinMode(_dataPin, INPUT);
+  pinMode(_clockPin, OUTPUT);
+  val = shiftIn(_dataPin, _clockPin, 8);
+  val *= 256;
+
+  // Send the required ack
+  pinMode(_dataPin, OUTPUT);
+  digitalWrite(_dataPin, HIGH);
+  digitalWrite(_dataPin, LOW);
+  digitalWrite(_clockPin, HIGH);
+  digitalWrite(_clockPin, LOW);
+
+  // Get the least significant bits
+  pinMode(_dataPin, INPUT);
+  val |= shiftIn(_dataPin, _clockPin, 8);
+
+  return val;
+}
+
+/**
+*/
+void Camara::skipCrcSHT(int _dataPin, int _clockPin)
+{
+  // Skip acknowledge to end trans (no CRC)
+  pinMode(_dataPin, OUTPUT);
+  pinMode(_clockPin, OUTPUT);
+
+  digitalWrite(_dataPin, HIGH);
+  digitalWrite(_clockPin, HIGH);
+  digitalWrite(_clockPin, LOW);
+}
